d3240f72628b019e19a3793a63ff8cc09146d07b
Leonardo-Campos-2007 <leonardogomescampos2007@gmail.com>
2025-11-01 18:01:52 -0300

    Imprimir e variaveis

---
+++ Curso C++/imprimir.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+// Programa simples em C++ que imprime mensagens no console
+
+int main() {
+    std::cout << "Ola, Mundo!" << std::endl;
+    std::cout << "Este eh meu primeiro progama em C++!" << std::endl;
+
+    return 0;
+}
---
+++ Curso C++/variaveis.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+    int x = 10;
+    int y = 20;
+    int soma = x + y;
+
+    std::cout << "A soma de " << x << " e " << y << " eh " << soma << std::endl;
+    
+    return 0;
+}
